071c15c19440b757a292147c717988d9eafade34
Chris Knoblauch <cjknobla@gmail.com>
2026-02-22 21:43:16 +0000

    create dim tables

--- models/marts/dim_vendors.sql
+++ models/marts/dim_vendors.sql
@@ -1,1 +1,10 @@
-select 1
+with trips_unioned as (
+    select * from {{ ref('int_trips_union') }}
+),
+
+vendors as (
+    select distinct vendor_id,
+    {{ get_vendor_names('vendor_id') }} as vendor_name from trips_unioned
+)
+
+select * from vendors
